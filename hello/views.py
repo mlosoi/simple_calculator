ef55fb5ff5be16a658a24334b4815cb3aae2ac7d
Markus Losoi <markus.losoi@gmail.com>
2019-03-10 20:03:45 +0200

    An URL parameter test

--- hello/views.py
+++ hello/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('Hello from Python! {0}'.format(request.GET['query'])
     # return render(request, "index.html")
