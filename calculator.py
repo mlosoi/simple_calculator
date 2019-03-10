e8e69536ce797e3b198761ad3ed27e42027c4c00
Markus Losoi <markus.losoi@gmail.com>
2019-03-10 14:08:17 +0200

    Moved the input expression tokenizer from the calculator module into the calculator_token module. Added utility functions into the Token class. These make it easier to check the type of a token. Added two new test expressions of which the second revealed a bug in infix_to_postfix()

--- calculator.py
+++ calculator.py
@@ -60,64 +60,20 @@ def validate_input(expression):
 
     return True
 
-def tokenize_expression(expression):
-    tokens = []
-
-    reading_number = False
-    current_number = None
-
-    i_current_character = 0
-
-    for character in expression:
-        if character.isdigit():
-            reading_number = True
-
-            if current_number == None:
-                current_number = character
-            else:
-                current_number += character
-
-        else:
-            if reading_number:
-                tokens.append(Token(Token.Type.NUMBER, float(current_number)))
-
-                reading_number = False
-                current_number = None
-
-            if character == Token.Identifier.OPERATOR_ADDITION or character == Token.Identifier.OPERATOR_SUBTRACTION or character == Token.Identifier.OPERATOR_MULTIPLICATION or character == Token.Identifier.OPERATOR_DIVISION:
-                # Consider the special case in which a subexpression begins with negation. In such cases, the minus operator should be converted to -1 and a multiplication tokens
-                if character == Token.Identifier.OPERATOR_SUBTRACTION and (i_current_character == 0 or (i_current_character > 0 and expression[i_current_character - 1] == Token.Identifier.OPENING_PARENTHESIS)):
-                    tokens.append(Token(Token.Type.NUMBER, -1))
-                    tokens.append(Token(Token.Type.OPERATOR, Token.Identifier.OPERATOR_MULTIPLICATION))
-
-                else:
-                    tokens.append(Token(Token.Type.OPERATOR, character))
-            elif character == Token.Identifier.OPENING_PARENTHESIS:
-                tokens.append(Token(Token.Type.OPENING_PARENTHESIS, character))
-            elif character == Token.Identifier.CLOSING_PARENTHESIS:
-                tokens.append(Token(Token.Type.CLOSING_PARENTHESIS, character))
-
-        i_current_character += 1
-
-    if reading_number:
-        tokens.append(Token(Token.Type.NUMBER, float(current_number)))
-
-    return tokens
-
 def infix_to_postfix(infix_expression):
     postfix_expression = []
     operator_stack = []
 
     for token in infix_expression:
-        if token.type == Token.Type.NUMBER:
+        if token.is_number():
             postfix_expression.append(token)
-        elif token.type == Token.Type.OPERATOR:
-            if token.datum == Token.Identifier.OPERATOR_ADDITION or token.datum == Token.Identifier.OPERATOR_SUBTRACTION:
+        elif token.is_operator():
+            if token.is_addition_operator() or token.is_subtraction_operator():
                 # Pop operators from the stack and append them to the postfix expression until the top operator is either an addition or subtraction operator, i.e., has the same precedence level as the current operator
                 if len(operator_stack) > 0:
                     top_operator = operator_stack[-1]
 
-                    while top_operator != None and (top_operator.datum == Token.Identifier.OPERATOR_MULTIPLICATION or top_operator.datum == Token.Identifier.OPERATOR_SUBTRACTION):
+                    while top_operator != None and (top_operator.is_multiplication_operator() or top_operator.is_division_operator()):
                         postfix_expression.append(operator_stack.pop())
 
                         if len(operator_stack) > 0:
@@ -127,12 +83,12 @@ def infix_to_postfix(infix_expression):
 
             operator_stack.append(token)
 
-        elif token.type == Token.Type.OPENING_PARENTHESIS:
+        elif token.is_opening_parenthesis():
             operator_stack.append(token)
-        elif token.type == Token.Type.CLOSING_PARENTHESIS:
+        elif token.is_closing_parenthesis():
             top_operator = operator_stack.pop()
 
-            while top_operator.type != Token.Type.OPENING_PARENTHESIS:
+            while not top_operator.is_opening_parenthesis():
                 postfix_expression.append(top_operator)
 
                 top_operator = operator_stack.pop()
@@ -147,21 +103,21 @@ def evaluate_postfix(postfix_expression):
     number_stack = []
 
     for token in postfix_expression:
-        if token.type == Token.Type.NUMBER:
+        if token.is_number():
             number_stack.append(token)
-        elif token.type == Token.Type.OPERATOR:
+        elif token.is_operator():
             operand_2 = number_stack.pop()
             operand_1 = number_stack.pop()
 
             result = None
 
-            if token.datum == Token.Identifier.OPERATOR_ADDITION:
+            if token.is_addition_operator():
                 result = Token(Token.Type.NUMBER, operand_1.datum + operand_2.datum)
-            elif token.datum == Token.Identifier.OPERATOR_SUBTRACTION:
+            elif token.is_subtraction_operator():
                 result = Token(Token.Type.NUMBER, operand_1.datum - operand_2.datum)
-            elif token.datum == Token.Identifier.OPERATOR_MULTIPLICATION:
+            elif token.is_multiplication_operator():
                 result = Token(Token.Type.NUMBER, operand_1.datum * operand_2.datum)
-            elif token.datum == Token.Identifier.OPERATOR_DIVISION:
+            elif token.is_division_operator():
                 result = Token(Token.Type.NUMBER, operand_1.datum / operand_2.datum)
 
             number_stack.append(result)
--- calculator_token.py
+++ calculator_token.py
@@ -19,12 +19,80 @@ class Token:
     type = None
     datum = None
 
-    def __init__(token, type, datum):
-        token.type = type
-        token.datum = datum
+    def __init__(self, type, datum):
+        self.type = type
+        self.datum = datum
 
-    def __repr__(token):
-        return "(" + str(token.type) + ", " + str(token.datum) + ")"
+    def __repr__(self):
+        return "(" + str(self.type) + ", " + str(self.datum) + ")"
+
+    def is_number(self):
+        return self.type == Token.Type.NUMBER
+
+    def is_operator(self):
+        return self.type == Token.Type.OPERATOR
+
+    def is_addition_operator(self):
+        return self.type == Token.Type.OPERATOR and self.datum == Token.Identifier.OPERATOR_ADDITION
+
+    def is_subtraction_operator(self):
+        return self.type == Token.Type.OPERATOR and self.datum == Token.Identifier.OPERATOR_SUBTRACTION
+
+    def is_multiplication_operator(self):
+        return self.type == Token.Type.OPERATOR and self.datum == Token.Identifier.OPERATOR_MULTIPLICATION
+
+    def is_division_operator(self):
+        return self.type == Token.Type.OPERATOR and self.datum == Token.Identifier.OPERATOR_DIVISION
+
+    def is_opening_parenthesis(self):
+        return self.type == Token.Type.OPENING_PARENTHESIS
+
+    def is_closing_parenthesis(self):
+        return self.type == Token.Type.CLOSING_PARENTHESIS
+
+def tokenize_expression(expression):
+    tokens = []
+
+    reading_number = False
+    current_number = None
+
+    i_current_character = 0
+
+    for character in expression:
+        if character.isdigit():
+            reading_number = True
+
+            if current_number == None:
+                current_number = character
+            else:
+                current_number += character
+
+        else:
+            if reading_number:
+                tokens.append(Token(Token.Type.NUMBER, float(current_number)))
+
+                reading_number = False
+                current_number = None
+
+            if character == Token.Identifier.OPERATOR_ADDITION or character == Token.Identifier.OPERATOR_SUBTRACTION or character == Token.Identifier.OPERATOR_MULTIPLICATION or character == Token.Identifier.OPERATOR_DIVISION:
+                # Consider the special case in which a subexpression begins with negation. In such cases, the minus operator should be converted to -1 and a multiplication tokens
+                if character == Token.Identifier.OPERATOR_SUBTRACTION and (i_current_character == 0 or (i_current_character > 0 and expression[i_current_character - 1] == Token.Identifier.OPENING_PARENTHESIS)):
+                    tokens.append(Token(Token.Type.NUMBER, -1))
+                    tokens.append(Token(Token.Type.OPERATOR, Token.Identifier.OPERATOR_MULTIPLICATION))
+
+                else:
+                    tokens.append(Token(Token.Type.OPERATOR, character))
+            elif character == Token.Identifier.OPENING_PARENTHESIS:
+                tokens.append(Token(Token.Type.OPENING_PARENTHESIS, character))
+            elif character == Token.Identifier.CLOSING_PARENTHESIS:
+                tokens.append(Token(Token.Type.CLOSING_PARENTHESIS, character))
+
+        i_current_character += 1
+
+    if reading_number:
+        tokens.append(Token(Token.Type.NUMBER, float(current_number)))
+
+    return tokens
 
 def tokenized_expression_to_str(tokenized_expression):
     return reduce(lambda token_1_datum, token_2_datum: '{0} {1}'.format(token_1_datum, token_2_datum), map(lambda token: token.datum, tokenized_expression))
