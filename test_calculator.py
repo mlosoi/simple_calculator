0e055a45d0bc35bfa3c917a77a2570bb1feafe6b
Markus Losoi <markus.losoi@gmail.com>
2019-03-11 22:34:06 +0200

    Added a test case for testing the web service

--- test_calculator.py
+++ test_calculator.py
@@ -7,42 +7,43 @@ import calculator
 
 calculator.print_debug_output = False
 
-class TestCalculator(TestCase):
-    test_expressions = [
-        '-(-(-(-10)))',
-        '-4',
-        '-1*1234*(((-1)))*(-1)+1234',
-        '-1*1234*(((-1)))*(-1)',
-        '1+1+(1+3)+4*5',
-        '-(2 * (23/(33))- 23 * (23))',
-        '-2 * (23/(33))- 23 * (23)',
-        '2 * (23/(33))- 23 * (23)',
-        '1+1',
-        '(((-3)))',
-        '-(5+6)',
-        '-(-1)',
-        '(((-3)))',
-        '-1*1234*(((-1)))*(-1)',
-        '-1*1234*(((-1)))/(-1)',
-        '-1*1234*(((-1)))',
-        '1+1+1+3',
-        '(1+1+1-(3+4)*5)/5',
-        '1+1+1-(3+4)*5',
-        '1 / 1 / 1 + 5',
-        '1+2+3+4-4-3-2-1',
-        '1*2*3*4/4/3/2/1'
-    ]
+test_expressions = [
+    '-(-(-(-10)))',
+    '-4',
+    '-1*1234*(((-1)))*(-1)+1234',
+    '-1*1234*(((-1)))*(-1)',
+    '1+1+(1+3)+4*5',
+    '-(2 * (23/(33))- 23 * (23))',
+    '-2 * (23/(33))- 23 * (23)',
+    '2 * (23/(33))- 23 * (23)',
+    '1+1',
+    '(((-3)))',
+    '-(5+6)',
+    '-(-1)',
+    '(((-3)))',
+    '-1*1234*(((-1)))*(-1)',
+    '-1*1234*(((-1)))/(-1)',
+    '-1*1234*(((-1)))',
+    '1+1+1+3',
+    '(1+1+1-(3+4)*5)/5',
+    '1+1+1-(3+4)*5',
+    '1 / 1 / 1 + 5',
+    '1+2+3+4-4-3-2-1',
+    '1*2*3*4/4/3/2/1'
+]
 
+class TestCalculator(TestCase):
     def test_calculator(self):
         all_values_correct = True
 
-        for test_expression in self.test_expressions:
+        for test_expression in test_expressions:
             print('Testing the expression: {0}...'.format(test_expression))
 
             calculated_value = calculator.calculate_value(test_expression)
 
             reference_value = float(eval(test_expression))
 
+            # Avoid naive floating point comparison by allowing the calculated and the reference value to be close enough. Otherwise, the accumulation of  floating point errors may lead to incorrect deduction
             if math.isclose(calculated_value, reference_value):
                 print('PASS: The calculated value {0} and the reference value {1} are close enough'.format(calculated_value, reference_value))
             else:
---
+++ test_web_service.py
@@ -0,0 +1,60 @@
+import base64
+import json
+import math
+
+from unittest import TestCase
+from unittest import main
+from urllib.request import urlopen
+
+import test_calculator
+
+class TestWebService(TestCase):
+
+    web_service_endpoint = 'https://mlosoi-simple-calculator.herokuapp.com/calculus'
+
+    query_url_parameter_identifier = 'query'
+
+    response_error_identifier = 'error'
+
+    response_value_identifier = 'value'
+
+    def test_web_service(self):
+        all_values_correct = True
+
+        for test_expression in test_calculator.test_expressions:
+            print('Testing the expression: {0}...'.format(test_expression))
+
+            base64_encoded_test_expression = base64.b64encode(bytes(test_expression, 'utf-8')).decode('utf-8')
+
+            print('Base64-encoded expression: {0}'.format(base64_encoded_test_expression))
+
+            api_call = '{0}?{1}={2}'.format(self.web_service_endpoint, self.query_url_parameter_identifier, base64_encoded_test_expression)
+
+            print('Calling the API in the URL: {0}'.format(api_call))
+
+            response = json.loads(urlopen(api_call).read().decode('utf-8'))
+
+            print('Response: {0}'.format(response))
+
+            if not response[self.response_error_identifier]:
+                calculated_value = response[self.response_value_identifier]
+
+                reference_value = float(eval(test_expression))
+
+                # Avoid naive floating point comparison by allowing the calculated and the reference value to be close enough. Otherwise, the accumulation of  floating point errors may lead to incorrect deduction
+                if math.isclose(calculated_value, reference_value):
+                    print('PASS: The calculated value {0} and the reference value {1} are close enough'.format(calculated_value, reference_value))
+                else:
+                    print('FAIL: The calculated value {0} and the reference value {1} are not close enough'.format(calculated_value, reference_value))
+
+                    all_values_correct = False
+
+            else:
+                print('FAIL: There was an error evaluating the expression')
+
+                all_values_correct = False
+
+        self.assertTrue(all_values_correct)
+
+if __name__ == '__main__':
+    main()
