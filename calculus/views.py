905ee1274557cd3e8348caf677d73212541ba6c5
Markus Losoi <markus.losoi@gmail.com>
2019-03-13 18:48:37 +0200

    Parameterized web service response JSON parameter names

--- calculus/views.py
+++ calculus/views.py
@@ -9,6 +9,8 @@ sys.path.append('..')
 
 from calculator import calculate_value
 
+# Response JSON attribute names {
+
 query_url_parameter_identifier = 'query'
 
 response_error_identifier = 'error'
@@ -17,27 +19,35 @@ response_message_identifier = 'message'
 
 response_value_identifier = 'value'
 
+# Response JSON attribute names }
+
 def calculus(request):
 
     query_url_parameter_value = None
 
     try:
+        # Extract the URL parameter value
         query_url_parameter_value = request.GET[query_url_parameter_identifier]
+
     except KeyError:
-        return HttpResponse(json.dumps({response_error_identifier: True, response_message_identifier: 'No {0} URL parameter was given. Supply the parameter'.format(query_url_parameter_identifier)}), content_type = 'application/json')
+        return HttpResponse(json.dumps({response_error_identifier: True, response_message_identifier: 'No \'{0}\' URL parameter was given. Supply the parameter'.format(query_url_parameter_identifier)}), content_type = 'application/json')
 
     infix_exprssion = None
 
     try:
+        # Base64 decode the URL parameter value to an infix expresion
         infix_expression = b64decode(query_url_parameter_value).decode('UTF-8')
+
     except:
-        return HttpResponse(json.dumps({'error': True, 'message': 'Failed to decode input expression from the {0} URL parameter. Check the parameter'.format(query_url_parameter_identifier)}), content_type = 'application/json')
+        return HttpResponse(json.dumps({response_error_identifier: True, response_message_identifier: 'Failed to decode input expression from the \'{0}\' URL parameter. Check the parameter'.format(query_url_parameter_identifier)}), content_type = 'application/json')
 
     value = None
 
     try:
+        # Calculate the given expression
         value = calculate_value(infix_expression)
-    except:
-        return HttpResponse(json.dumps({'error': True, 'message': 'Failed to calculate the expression {0}'.format(infix_expression)}), content_type = 'application/json')
 
-    return HttpResponse(json.dumps({'error': False, 'value': value}), content_type = 'application/json')
+    except:
+        return HttpResponse(json.dumps({response_error_identifier: True, response_message_identifier: 'Failed to calculate the expression {0}'.format(infix_expression)}), content_type = 'application/json')
+
+    return HttpResponse(json.dumps({response_error_identifier: False, response_value_identifier: value}), content_type = 'application/json')
